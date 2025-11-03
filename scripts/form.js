778f0c5a9db1f9dbe68e83ffddee6d9ed3217694
Juls00 <juliagimena00@alumnos.cei.es>
2025-11-03 10:24:06 +0100

    add terminal and button

---
+++ scripts/form.js
@@ -0,0 +1,32 @@
+
+function restartGame() {
+    Swal.fire({
+        title: "Do you want to go to the homepage?",
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: "Yes",
+        denyButtonText: `No`
+    }).then((result) => {
+
+        if (result.isConfirmed) {
+            Swal.fire("Saved!", "", "success");
+        } else if (result.isDenied) {
+            Swal.fire("Changes are not saved", "", "info");
+        }
+    });
+}
+
+function toggleMute() {
+            const icon = document.querySelector('#muteBtn i');
+            icon.classList.toggle('fa-volume-xmark');
+            icon.classList.toggle('fa-volume-high');
+}
+
+
+
+
+
+
+
+
+
