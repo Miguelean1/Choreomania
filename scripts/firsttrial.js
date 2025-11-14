a1dcf3b5a6469c86f24fe79169e19c664a3dfced
CesarMed06 <cmedsag@alumnos.cei.es>
2025-11-14 20:34:31 +0100

    Added music to view firsttrial (without loop)

--- scripts/firsttrial.js
+++ scripts/firsttrial.js
@@ -108,6 +108,7 @@ document.addEventListener(
 
             // Si el sorteo ya terminó, permitir clic que redirige al componente destino
             if (raffleFinished) {
+                stopMusic()
                 // redirige a la ruta configurada
                 window.location.href = POST_RAFFLE_REDIRECT;
                 return;
@@ -138,6 +139,7 @@ document.addEventListener(
 );
 
 function returnHome() {
+    stopMusic();
     Swal.fire({
         title: "Do you want to go to the homepage?",
         showDenyButton: true,
@@ -151,12 +153,6 @@ function returnHome() {
             Swal.fire("Changes are not saved", "", "info");
         }
     });
-}
-
-function muteMusic() {
-    const icon = document.querySelector("#muteBtn i");
-    icon.classList.toggle("fa-volume-xmark");
-    icon.classList.toggle("fa-volume-high");
 }
 
 function titleStyle() {
